79e1c17968881ffa9681dfe04c6ddeea5324e0d4
Sachin Sahu <sachinssahu@gmail.com>
2026-08-31 00:31:51 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,21 @@
 
 console.log(3 + 3); 
 console.log("Hello World !!");
+
+let name = "Sachin"
+let age = 29
+let isLoggedIn = true
+
+// number => 2 to power 23
+// bigint
+// string => can be in decalared single or double quotes both
+// boolean => true/false
+// null => standalone value
+// undefined => variable is decalared but currently values is not defined
+// symbol => unique, mostly used in FE and Figma
+// object => 
+    
+
+console.log(typeof "Sachin");
+console.log(typeof age);
+console.log(typeof null);
