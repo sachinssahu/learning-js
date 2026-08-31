8b53d456489697c14249fea8fc7817aa40004f11
Sachin Sahu <sachinssahu@gmail.com>
2026-08-31 00:49:03 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "sachin" // 69 "69" "69xxx" null undefined true false "Sachin"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+69 => 69
+"69" => 69
+"69xxx" => NaN
+true => 1
+false => 0
+null => 0
+undefined => NaN
+*/
+
+let isLoggedIn = 2345
+
+let BoolLoggedIn = Boolean(isLoggedIn);
+
+console.log(BoolLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Sachin" => true
+// null, undefined => false
+// 
