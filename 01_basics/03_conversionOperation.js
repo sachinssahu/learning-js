5287b6f74f906e051c2743eb86d58fdd77cb7546
Sachin Sahu <sachinssahu@gmail.com>
2026-09-04 18:44:38 +0000

    pre and postfix increment and decrement

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -51,7 +51,21 @@ let str3 = str1 + str2
 
 // console.log(str1+str2);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 3 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //string
+// console.log(1 + 3 + "2"); // number add then string
+
+// console.log(+true);
+// console.log(true+); syntax error
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter);
+console.log(gameCounter++)
+console.log(gameCounter);
