45e818e626c695d0fb106644a5f07423c3e24460
Sachin Sahu <sachinssahu@gmail.com>
2026-09-04 20:10:04 +0000

    stack n heap memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -42,3 +42,23 @@ You do not need to explicitly declare data types when writing code
 
 // Documentation: https://developer.mozilla.org/en-US/
 // ECMA documention: https://262.ecma-international.org/
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Memory and its types
+// Stack (Primitive, copy of decalared variable), Heap (Non-Primitive, refernce of original value)
+
+let myAnimeName = "Sachin"
+let anotherName = myAnimeName
+anotherName = "Alucard"
+
+console.log(myAnimeName, "\n", anotherName);
+
+let userOne = {
+    email: "sachinssahu@gmail.com",
+    upi: "8109188026@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "alucard@gmail.com"
+
+console.log(userOne.email, userTwo.email);
