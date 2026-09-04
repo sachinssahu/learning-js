205fb991def647fd3bd8105265ed9f1aa9cad65f
Sachin Sahu <sachinssahu@gmail.com>
2026-09-04 18:32:39 +0000

    operations video 7

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,11 +22,36 @@ let isLoggedIn = 2345
 
 let BoolLoggedIn = Boolean(isLoggedIn);
 
-console.log(BoolLoggedIn);
+// console.log(BoolLoggedIn);
 
 // 1 => true
 // 0 => false
 // "" => false
 // "Sachin" => true
 // null, undefined => false
-// 
+
+
+/************************** Operations **************************/
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(3%2);
+
+
+let str1 = "hello"
+let str2 = " ** Sachin"
+let str3 = str1 + str2
+
+// console.log(str1+str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 3 + "2");
