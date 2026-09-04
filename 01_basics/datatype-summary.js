b1ed1138fa016529be86bc887299adfdeb161cfb
Sachin Sahu <sachinssahu@gmail.com>
2026-09-04 19:32:27 +0000

    new branch

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+// 7 datatype: Number, String, Boolean, null, undefined, Symbol, BigInt
+
+const score = false;
+const string = "Loop"
+let BigInt = 345678909876543456789;
+
+console.log(typeof BigInt);
+
+// Refrence (Non Premitive)
+// Array, Objects, Functions
+
+
+
+/*
+Note: JavaScript is a dynamically typed language.
+
+According to the MDN Web Docs Glossary, this means the interpreter assigns types to variables 
+automatically at runtime based on their value.
+You do not need to explicitly declare data types when writing code 
+*/
+
+
+// Documentation: https://developer.mozilla.org/en-US/
+// ECMA documention: https://tc39.es/ecma262/
