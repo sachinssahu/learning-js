7b96ab0b9d7de23f28afc6a6791a869def4021af
Sachin Sahu <sachinssahu@gmail.com>
2026-09-07 11:58:42 +0000

    do while loop

---
+++ 05-iterations/02-do-while-loop.js
@@ -0,0 +1,22 @@
+// while loop
+let i = 0
+while (i <= 10) {
+    console.log(`Value of index is ${i}`);
+    i += 2; // i = i + 2
+}
+
+let myArray = ["Naruto", "Sasuke", "Kurama"]
+let arr = 0;
+
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do-while loop
+let score = 0
+
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1;
+} while (score <=10);
